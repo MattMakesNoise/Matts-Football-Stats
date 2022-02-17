7195506fa41969f2a794d90bdb35037be66e2cf2
MattMakesNoise <72995030+MattMakesNoise@users.noreply.github.com>
2022-02-17 09:10:12 +0000

    Update TeamBanner.js

--- src/components/TeamBanner/TeamBanner.js
+++ src/components/TeamBanner/TeamBanner.js
@@ -1,29 +1,8 @@
 import React from "react";
-import axios from "axios";
 import {teamIdsFixtures, formatDate, formatTime} from "../../helpers";
 import styles from "./TeamBanner.module.css";
 
 const TeamBanner = (props) => { 
-    const options = {
-        method: 'GET',
-        url: 'https://api-football-beta.p.rapidapi.com/fixtures',
-        params: {
-            to: '2022-06-30',
-            season: '2021',
-            league: '39',
-            from: '2021-08-01'
-        },
-        headers: {
-            'x-rapidapi-host': 'api-football-beta.p.rapidapi.com',
-            'x-rapidapi-key': 'f70403a975msh4ae717f35387ed9p18ff5djsn9a1119e794e8'
-        }
-    };
-    
-    axios.request(options).then(function (res) {
-        localStorage.setItem("apiDataFixtures", JSON.stringify(res.data.response));
-    }).catch(function (error) {
-        console.error(error);
-    });
 
     const fixtures = JSON.parse(localStorage.getItem("apiDataFixtures"));
 
